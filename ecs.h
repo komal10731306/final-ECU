dcceb9fab604192d46c879297434973187bf429e
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-04 17:23:09 +0530

    Create ecs.h

---
+++ ecs.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include<time.h>
+
+#define PORT 12345
+#define MAX_BUFFER_SIZE 1024
