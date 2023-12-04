ca67b862144a3dbcd5ccfd92214fca139795f591
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-04 17:26:25 +0530

    Create fault.h

---
+++ fault.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include<sys/ipc.h>  
+#include<sys/shm.h>
+#include<stdint.h>
+#include<time.h>
+#include<mqueue.h>
+
+// Define signals for faults
+struct FAULT
+{
+    int pid;
+	int fuelq;
+};
+
+struct SDF{
+    float temperature;
+    float speed;
+    float pressure;
+    float fused;
+};
+struct SDF *memory;
