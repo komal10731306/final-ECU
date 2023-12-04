f6a2d67a52e1aa1eb7862bae4bb9ca85653f636e
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-04 17:25:55 +0530

    Update FAULT.c

--- FAULT.c
+++ FAULT.c
@@ -1,27 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include<sys/ipc.h>  
-#include<sys/shm.h>
-#include<stdint.h>
-#include<time.h>
-#include<mqueue.h>
-
-// Define signals for faults
-struct FAULT
-{
-	int pid;
-	int fuelq;
-};
-
-struct SDF{
-    float temperature;
-    float speed;
-    float pressure;
-    float fused;
-};
-struct SDF *memory;
+#include "fault.h"
 // Signal handlers
 }
 int main() 
