b30cc34ce23390bffe81e74300efaa75e74ebe07
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-04 17:28:08 +0530

    Create ficm.h

---
+++ ficm.h
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/ipc.h>  
+#include<sys/shm.h>
+#include<stdint.h>
+#include<time.h>
+struct FICM
+{
+	int fuel;
+	char time[30];
+};
+struct FAULT
+{
+	int pid;
+	int fuelq;
+};
+
+struct SDF{
+    float temperature;
+    float speed;
+    float pressure;
+    float fused;
+};
+int calculate(int ,int );
+int random_input(int ,int );
