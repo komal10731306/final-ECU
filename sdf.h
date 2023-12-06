a729d82fac8981df8e2eb04169b63175bf85e4a8
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-06 19:19:06 +0530

    Create sdf.h

---
+++ sdf.h
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+// function declaration
+struct SDF input();
