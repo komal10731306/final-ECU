1805959fd6cde79d0109c71761f480768c9204e3
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-06 17:50:04 +0530

    Update ecs.h

--- ecs.h
+++ ecs.h
@@ -9,5 +9,7 @@
 
 #define PORT 12345
 #define MAX_BUFFER_SIZE 1024
+//handle_client used to send info from server to client 
 void handle_client(int );
+//adjusting to minimum value (range)
 uint16_t getupdatetominimum(uint16_t ,char *);
