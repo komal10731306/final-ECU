f1691cceb5b0d0c298cd9d9dc51a7281c62a3684
Thejus GC <99389127+thejusgc@users.noreply.github.com>
2023-12-06 18:23:22 +0530

    Update FAULT.c

--- FAULT.c
+++ FAULT.c
@@ -2,16 +2,19 @@
 // Signal handlers
 int main() 
 {
+  //shared memory 
   int shmid,key_t=23452;
   struct FICM  *shared_memory;
+  //shared memory of ficm 
   shmid=shmget((key_t),1024,0666| IPC_CREAT);
-  //printf("KEY OF THESHARED MEMORY IS %d\n",shmid);
+  
   shared_memory =(struct FICM*)shmat(shmid,NULL,0);
     if(shmid==-1)
     {
       printf("SHARED MEMORY NOT ASSIGNED");
       exit(1);
     }
+  //shared memory of sdf
     memory=(struct SDF*)shmat(shmid,NULL,0);
     //printf("\nTEMPERATURE READ FROM  : %.2f\n",memory->temperature);
     int shmidnew,key_t1=11111;
@@ -26,13 +29,15 @@ int main()
     {
       printf("FICM pid %d\n",smem_fault->pid);
       printf("Temperature is too high. Fault detected!\n");
-        kill(smem_fault->pid,SIGTERM);
+      //kill the process based on its pid  
+      kill(smem_fault->pid,SIGTERM);
     }
 
     else if (smem_fault->fuelq==1) 
     {
       printf("ECU pid %d\n",smem_fault->pid);
       printf("Fuel leakage detected. Fault detected!\n");
+      //kill the process based on its pid 
         kill(smem_fault->pid,SIGKILL);
     }
     else
